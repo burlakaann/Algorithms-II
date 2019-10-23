5f7efec3373decdb89c25be258e0a6c6ef90e9dc
burlakaann <36855034+burlakaann@users.noreply.github.com>
2019-10-23 12:53:53 +0200

    Create graph2.cpp

---
+++ graph2.cpp
@@ -0,0 +1,251 @@
+#include <iostream>
+
+using namespace std;
+
+class Graf{
+private:
+    int size;
+    int n;
+    int n_lst;
+    int *kolejka;
+    struct sasiad{
+        int numer;
+        double prowizja1;
+        double prowizja2;
+        sasiad *nastepny;
+    };
+    struct wierzcholek{
+        double suma1;
+        double suma2;
+        bool bajtcoin;
+        sasiad *sasiedzi;
+    };
+    wierzcholek *tab;
+    void push_lst(int);
+    int find1();
+    int find2();
+    void pop(int);
+    bool empty();
+    int *sciezka;
+    bool bf;
+    void change_size();
+public:
+    Graf(int, bool*, int );
+    Graf(){}
+    void push(int , int, double, double);
+    void bfs1(int );
+    void bfs2(int );
+    void bfs3(int );
+    void znajdz(int);
+};
+void Graf::change_size(){
+	size*=2;
+	int *new_tab;
+	new_tab = new int[size];
+	for(int i = 0; i < n_lst; i++){
+		new_tab[i] = kolejka[i];
+	}
+	delete kolejka;
+	kolejka = new_tab;
+}
+
+void Graf::push_lst(int x){
+    if(size < n_lst)
+        change_size();
+    kolejka[n_lst] = x;
+    n_lst++;
+}
+void Graf::pop(int x){
+    for(int i = 0; i < n_lst; i++){
+        if(kolejka[i] == x){
+            for(int j = i; j < n_lst-1; j++){
+                kolejka[j] = kolejka[j+1];
+            }
+            n_lst--;
+        }
+    }
+}
+int Graf::find1(){
+    int max = 0;
+    int max_index;
+    for(int i = 0; i < n_lst; i++){
+        if(tab[kolejka[i]].suma1 > max){
+            max = tab[i].suma1;
+            max_index = i;
+        }
+    }
+    return kolejka[max_index];
+}
+
+int Graf::find2(){
+    int max = 0;
+    int max_index;
+    for(int i = 0; i < n_lst; i++){
+        if(tab[kolejka[i]].suma2 > max){
+            max = tab[i].suma2;
+            max_index = i;
+        }
+    }
+    return kolejka[max_index];
+}
+bool Graf::empty(){
+    if(n_lst == 0)
+        return true;
+    return false;
+}
+Graf::Graf(int n, bool *czy_bc, int m){
+    this->n = n;
+    tab = new wierzcholek[n];
+    for(int i = 0; i < n; i++){
+        tab[i].sasiedzi = NULL;
+        tab[i].suma1 = 0;
+        tab[i].suma2 = 0;
+        tab[i].bajtcoin = czy_bc[i];
+    }
+    n_lst = 0;
+    size = m;
+    kolejka = new int[size];
+    bf = true;
+    sciezka = new int[n];
+}
+void Graf::bfs1(int v){
+    n_lst = 0;
+    sciezka[v] = 0;
+    sasiad *z_p;
+    tab[v].suma1 = 100000;
+    push_lst(v);
+
+    while(!empty()){
+        v = find1();
+        pop(v);
+        z_p = tab[v].sasiedzi;
+        while(z_p != NULL ){
+            if( tab[v].suma1 - (tab[v].suma1 * z_p->prowizja1) > tab[z_p->numer].suma1){
+                tab[z_p->numer].suma1 = tab[v].suma1 - (tab[v].suma1 * z_p->prowizja1);
+                if(sciezka[v] + 1 <= n-1){
+                    push_lst(z_p->numer);
+                    sciezka[z_p->numer] = sciezka[v] + 1;
+                }
+                else{
+                    bf = false;
+                    return ;
+                }
+            }
+            z_p = z_p->nastepny;
+        }
+    }
+}
+
+void Graf::bfs2(int v){
+    sciezka[v] = 0;
+    n_lst = 0;
+    tab[v].suma2 = 100000;
+    push_lst(v);
+    sasiad *z_p;
+    while(!empty()){
+        v = find2();
+        pop(v);
+        z_p = tab[v].sasiedzi;
+        while(z_p != NULL ){
+            if(tab[v].suma2 - (tab[v].suma2 * z_p->prowizja2) > tab[z_p->numer].suma2){
+                tab[z_p->numer].suma2 = tab[v].suma2 - (tab[v].suma2 * z_p->prowizja2);
+                if(sciezka[v] + 1 <= n-1){
+                    push_lst(z_p->numer);
+                    sciezka[z_p->numer] = sciezka[v] + 1;
+                }
+                else{
+                    bf = false;
+                    return ;
+                }
+            }
+            z_p = z_p->nastepny;
+        }
+    }
+}
+
+void Graf::bfs3(int v){
+    n_lst = 0;
+    sciezka[v] = 0;
+    tab[v].suma1 *= 1.2;
+    push_lst(v);
+    sasiad *z_p;
+    while(!empty()){
+        v = find1();
+        pop(v);
+        z_p = tab[v].sasiedzi;
+        while(z_p != NULL ){
+            if(tab[v].suma1 - (tab[v].suma1 * z_p->prowizja1) > tab[z_p->numer].suma1){
+                tab[z_p->numer].suma1 = tab[v].suma1 - (tab[v].suma1 * z_p->prowizja1);
+                if(sciezka[v] + 1 <= n-1){
+                    push_lst(z_p->numer);
+                    sciezka[z_p->numer] = sciezka[v] + 1;
+                }
+                else{
+                    bf = false;
+                    return ;
+                }
+            }
+            z_p = z_p->nastepny;
+        }
+    }
+}
+
+void Graf::znajdz(int s){
+    bfs1(s);
+    bfs2(s);
+    int max = 0;
+    int max_index;
+    for(int i = 0 ; i< n; i++){
+        if(tab[i].bajtcoin && tab[i].suma1 + tab[i].suma2 > max){
+            max = tab[i].suma1 + tab[i].suma2;
+            max_index = i;
+        }
+    }
+    for(int i = 0 ; i< n; i++){
+        if(i != max_index){
+            tab[i].suma1 = 0;
+        }
+    }
+    bfs3(max_index);
+    if(bf)
+        cout << (int)tab[s].suma1;
+    else
+        cout << "MATEUSZU NIE KOMBINUJ!";
+}
+
+void Graf::push(int x, int y, double p1, double p2){
+    sasiad *s = new sasiad;
+    s->numer = y;
+    s->prowizja1 = p1;
+    s->prowizja2 = p2;
+    s->nastepny = tab[x].sasiedzi;
+    tab[x].sasiedzi = s;
+
+    s = new sasiad;
+    s->numer = x;
+    s->prowizja1 = p2;
+    s->prowizja2 = p1;
+    s->nastepny = tab[y].sasiedzi;
+    tab[y].sasiedzi = s;
+
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    int il_gield, il_krawedzi, wiodaca;
+    cin >> il_gield;
+    bool *status = new bool[il_gield];
+    for(int i = 0; i < il_gield; i++){
+        cin >> status[i];
+    }
+    cin >> il_krawedzi;
+    Graf g(il_gield, status, il_krawedzi);
+    int gielda1, gielda2;
+    double prow1, prow2;
+    for(int i = 0; i < il_krawedzi; i++){
+        cin >> gielda1 >> gielda2;
+        cin >> prow1 >> prow2;
+        g.push(gielda1, gielda2, prow1, prow2);
+    }
+    cin >> wiodaca;
+    g.znajdz(wiodaca);
+}

33b5536593ff95237ec2426e2b1837bd4def1788
burlakaann <36855034+burlakaann@users.noreply.github.com>
2019-10-23 13:15:07 +0200

    Create black_red_tree.cpp

---
+++ black_red_tree.cpp
@@ -0,0 +1,313 @@
+#include <iostream>
+
+using namespace std;
+
+class bst{
+public:
+    struct atrakc{
+        double klucz;
+        bool kolor;
+        atrakc *l_syn;
+        atrakc *p_syn;
+        atrakc *ojciec;
+        string pseud;
+    };
+    atrakc *root;
+public:
+    bst();
+    atrakc* search1(double);
+    void insert1(double, string);
+};
+bst::bst(){
+    root = NULL;
+}
+
+bst::atrakc* bst::search1(double atr){
+    atrakc *z_p = root;
+    if(z_p != NULL){
+        while(z_p->p_syn != NULL){
+            z_p = z_p->p_syn;
+        }
+    }
+    else{
+        return z_p;
+    }
+
+    if(z_p->klucz >= atr){
+        return z_p;
+    }
+    else{
+        return NULL;
+    }
+}
+void bst::insert1(double atr, string pseud){
+    atrakc *v1, *v2;
+    atrakc *poprzedni;
+    v2 = root;
+    if (root == NULL){
+        v1 = new atrakc;
+        v1->ojciec = NULL;
+        v1->pseud = pseud;
+        v1->l_syn = v1->p_syn = NULL;
+        v1->klucz = atr;
+        v1->kolor = true;
+        root = v1;
+    }
+    else{
+        while(v2 != NULL && v2->klucz != atr){
+            poprzedni = v2;
+            if(v2->klucz < atr)
+                v2 = v2->p_syn;
+            else
+                v2 = v2->l_syn;
+        }
+        if(v2 == NULL){
+            v1 = new atrakc;
+            v1->l_syn = v1->p_syn = NULL;
+            v1->klucz = atr;
+            v1->kolor = true;
+            v1->ojciec = poprzedni;
+            v1->pseud = pseud;
+            if(poprzedni->klucz > v1->klucz){
+                poprzedni->l_syn = v1;
+            }
+            else if(poprzedni->klucz < v1->klucz){
+                poprzedni->p_syn = v1;
+            }
+            else{
+                v1->pseud = pseud;
+            }
+
+        }
+        else{
+            v2->pseud = pseud;
+        }
+    }
+}
+
+class BST{
+private:
+    struct wezel{
+        int klucz;
+        bool kolor;
+        wezel *l_syn;
+        wezel *p_syn;
+        wezel *ojciec;
+        bst *wartosc;
+    };
+    wezel *korzen;
+    wezel *straznik;
+public:
+    BST();
+    void search(int, double);
+    void insert(int, double, string);
+    void fix(wezel*);
+    void rotacja_l(wezel *);
+    void rotacja_p(wezel*);
+};
+BST::BST(){
+    straznik = new wezel;
+    straznik->kolor = false;
+    korzen = straznik;
+}
+void BST::fix(wezel *v1){
+    wezel *z_p, *prawy, *lewy;
+    if(v1->kolor == true){
+        if(v1 == korzen){
+            v1->kolor = false;
+        }
+        else{
+            if( v1->ojciec->kolor == true ){
+                z_p = v1->ojciec->ojciec;
+                if(z_p->l_syn == v1->ojciec){
+                    prawy = z_p->p_syn;
+                    if(prawy->kolor == true){
+                        v1->ojciec->kolor = false;
+                        prawy->kolor = false;
+                        z_p->kolor = true;
+                        v1 = z_p;
+                        fix(v1);
+                    }
+                    else{
+                        if(v1->ojciec->p_syn == v1){
+                            v1 = v1->ojciec;
+                            rotacja_l(v1);
+                        }
+                        v1->ojciec->kolor = false;
+                        z_p->kolor = true;
+                        rotacja_p(z_p);
+                    }
+                }
+                else{
+                    lewy = z_p->l_syn;
+                    if(lewy->kolor == true){
+                        v1->ojciec->kolor = false;
+                        lewy->kolor = false;
+                        z_p->kolor = true;
+                        v1 = z_p;
+                        fix(v1);
+                    }
+                    else{
+                        if(v1->ojciec->l_syn == v1){
+                            v1 = v1->ojciec;
+                            rotacja_p(v1);
+                        }
+                        rotacja_l(z_p);
+                        v1->ojciec->kolor = false;
+                        z_p->kolor = true;
+                    }
+                }
+            }
+        }
+    }
+}
+void BST::rotacja_l(wezel* v){
+    wezel *prawy;
+
+    prawy = v->p_syn;
+    v->p_syn = prawy->l_syn;
+    if(prawy->l_syn != straznik){
+        prawy->l_syn->ojciec = v;
+    }
+    prawy->ojciec = v->ojciec;
+    if(v->ojciec == straznik){
+        korzen = prawy;
+    }
+    else{
+        if(v->ojciec->l_syn == v){
+            v->ojciec->l_syn = prawy;
+        }
+        else{
+            v->ojciec->p_syn = prawy;
+        }
+    }
+    prawy->l_syn = v;
+    v->ojciec = prawy;
+}
+void BST::rotacja_p(wezel *v){
+    wezel *lewy;
+
+    lewy = v->l_syn;
+    v->l_syn = lewy->p_syn;
+    if(lewy->p_syn != straznik){
+        lewy->p_syn->ojciec = v;
+    }
+    lewy->ojciec = v->ojciec;
+    if(v->ojciec == straznik){
+        korzen = lewy;
+    }
+    else{
+        if(v->ojciec->l_syn == v){
+            v->ojciec->l_syn = lewy;
+        }
+        else{
+            v->ojciec->p_syn = lewy;
+        }
+    }
+    lewy->p_syn = v;
+    v->ojciec = lewy;
+}
+void BST::insert(int key, double wspol, string pseud){
+    wezel *v1, *v2;
+    wezel *poprzedni;
+
+    v2 = korzen;
+    if (korzen == straznik){
+        v1 = new wezel;
+        v1->l_syn = v1->p_syn = straznik;
+        v1->klucz = key;
+        v1->kolor = true;
+        v1->ojciec = straznik;
+        v1->wartosc = new bst;
+        v1->wartosc->insert1(wspol, pseud);
+        korzen = v1;
+        fix(v1);
+
+    }
+    else{
+        while(v2 != straznik && v2->klucz != key){
+            poprzedni = v2;
+            if(v2->klucz < key)
+                v2 = v2->p_syn;
+            else
+                v2 = v2->l_syn;
+        }
+        if(v2 == straznik){
+            v1 = new wezel;
+            v1->l_syn = v1->p_syn = straznik;
+            v1->klucz = key;
+            v1->kolor = true;
+            v1->wartosc = new bst;
+            v1->wartosc->insert1(wspol, pseud);
+            v1->ojciec = poprzedni;
+            if(poprzedni->klucz > key){
+                poprzedni->l_syn = v1;
+                fix(v1);
+            }
+            else if(poprzedni->klucz < key){
+                poprzedni->p_syn = v1;
+                fix(v1);
+            }
+            else{
+                v1->wartosc->insert1(wspol, pseud);
+            }
+        }
+        else{
+            v2->wartosc->insert1(wspol, pseud);
+        }
+    }
+}
+
+void BST::search(int x, double atrakc){
+    wezel *z_p = korzen;
+    while (z_p != straznik && z_p->klucz != x){
+        if(z_p->klucz < x){
+            z_p = z_p->p_syn;
+        }
+        else {
+            z_p = z_p->l_syn;
+        }
+    }
+    bst::atrakc *s ;
+
+    if(z_p == straznik){
+        cout << "-\n";
+    }
+    else{
+        s = z_p->wartosc->search1(atrakc);
+        if(s==NULL){
+            cout << "-\n";
+        }
+        else{
+            cout << s->pseud << "\n";
+        }
+    }
+}
+double oblicz_wspol(double x, int y){
+    return x/5*(int)(y/1000);
+}
+int main(){
+ios_base::sync_with_stdio(false);
+bst bs;
+BST bst;
+int il_operacji;
+cin >> il_operacji;
+char ch;
+double p1, atrakcyjnosc;
+int p2, koszt, il_pien;
+string pseudonim;
+for(int i = 0; i < il_operacji; i++){
+    cin >> ch;
+    if(ch == 'A'){
+        cin >> p1;
+        cin >> p2;
+        cin >> koszt;
+        cin >> pseudonim;
+        bst.insert(koszt, oblicz_wspol(p1,p2), pseudonim);
+    }
+    else{
+        cin >> il_pien >> atrakcyjnosc;
+        bst.search(il_pien, atrakcyjnosc);
+    }
+}
+}
